f4eb70910fb4f26df715f0ccf79f2c6472bd749f
andrey <andreyalgin@yandex.ru>
2023-01-14 19:10:34 +0300

    Implements Task 1

--- main.cpp
+++ main.cpp
@@ -10,10 +10,37 @@
 
 #include <unistd.h>
 
+void PrintBitstring(int input)
+{
+	size_t counter = sizeof(int)*8;
+	char output[counter + 1];
+	unsigned int value = static_cast<unsigned int>(input);
+
+	output[counter] = '\0';
+
+	while(counter--)
+	{
+		output[counter] = '0' + (value&1);
+
+		value >>= 1;
+	}
+
+	std::cout << input << ":	" << output << std::endl;
+}
 
 int main(int argc, const char* argv[])
 {
-	std::cout << "Quick test" << std::endl;
+	std::cout << "Task 1" << std::endl;
+	std::cout << "sizeof(int) " << sizeof(int)*8 << "-bits" << std::endl;
+
+	PrintBitstring(0);
+	PrintBitstring(1);
+	PrintBitstring(-1);
+	PrintBitstring(-23357);
+	PrintBitstring(23357);
+	PrintBitstring(-23358);
+	PrintBitstring(23358);
+
 
 	return 0;
 }
