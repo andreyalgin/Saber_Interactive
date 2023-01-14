78bb1518c40bdbf0ab978728bb1cd266545eecb8
andrey <andreyalgin@yandex.ru>
2023-01-14 16:22:49 +0300

    Test commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <array>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <unistd.h>
+
+
+int main(int argc, const char* argv[])
+{
+	std::cout << "Quick test" << std::endl;
+
+	return 0;
+}
